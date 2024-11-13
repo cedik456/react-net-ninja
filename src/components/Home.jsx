c8c35df3d5ddfa0086691d86e9a98a339e7fa00a
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-11-13 10:58:35 +0800

    Add custom hook to fetch mock data for home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../hooks/useFetch";
 
 function Home() {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not Fetch data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((e) => {
-          setError(e.message);
-          setIsPending(false);
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="Home">
