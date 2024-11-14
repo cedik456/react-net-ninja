fdacdcf037b4bffe217eb1ae9656737c9c91f0dc
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-11-14 17:20:43 +0800

    Add form and state values for adding blogs

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("jane"); // should have a default value when using select (option)
+
   return (
     <div className="create-blog">
-      <h2>Add a blog</h2>
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          required
+        />
+
+        <label>Body:</label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+
+        <label>Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="jane">Jane Smith</option>
+          <option value="alice">Alice Brown</option>
+          <option value="bob">Bob Johnson</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
