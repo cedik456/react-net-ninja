b7f10eaf34413bc9c5b17b67ea5d29db277a4cbc
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-11-14 16:32:33 +0800

    Reuse custom useFetch for blog details component

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,13 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 function BlogDetails() {
   const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <p>
-        Blog Details <br /> Id: {id}
-      </p>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h1>{blog.title}</h1>
+          <p>
+            Written by: <span>{blog.author}</span>
+          </p>
+          <body>{blog.body}</body>
+        </article>
+      )}
     </div>
   );
 }
